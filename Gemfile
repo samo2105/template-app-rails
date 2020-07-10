cf23876f640ecb7129a8c90c851b158fd36963ca
samo2105 <samuel.cea95@gmail.com>
2020-07-09 21:31:45 -0400

    trying heroku autodeploy

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ ruby '2.6.0'
 gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.4.2', group: :development
-gem 'pg', '~> 1.2.3', group: :production
+gem 'pg', '~> 1.2.3', group: [:production, :test]
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,24 @@
 class UsersController < ApplicationController
 
   def login
+    @users = [
+      {
+        nombre: 'Anita',
+        edad: 20
+      },
+      {
+        nombre: 'Pepito',
+        edad: 21
+      },
+      {
+        nombre: 'Hiliana',
+        edad: 22
+      },
+      {
+        nombre: 'Pierre',
+        edad: 23
+      },
+    ]
   end
 
 end
